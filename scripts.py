84e18f2bc8223b7a4761d6f495c161b0e50d4230
Suraj Sharma <suraj@ramailo.tech>
2025-03-09 14:36:43 +0545

    modified scripts file

--- scripts.py
+++ scripts.py
@@ -1,24 +1,23 @@
 import subprocess
 
 def run_command(command):
-    """Run a shell command and print its output."""
+    """Run a shell command and handle errors."""
     try:
         result = subprocess.run(command, check=True, shell=True, text=True, capture_output=True)
         print(result.stdout)
     except subprocess.CalledProcessError as e:
-        print(f"Error occurred while executing command: {command}")
-        print(e.stderr)
+        print(f"Error occurred: {e.stderr}")
 
 def main():
-    # List of commands to run
-    commands = [
-        "sudo apt-get install -y portaudio19-dev python3-pyaudio",
-        "pip install -r requirements.txt",
-        "python3 manage.py migrate"
-    ]
+    # Install system dependencies (only works if you have sudo permissions)
+    run_command("sudo apt-get update")
+    run_command("sudo apt-get install -y portaudio19-dev")
 
-    for command in commands:
-        run_command(command)
+    # Install Python dependencies
+    run_command("pip install -r requirements.txt")
+
+    # Run Django migrations
+    run_command("python3 manage.py migrate")
 
 if __name__ == "__main__":
     main()
